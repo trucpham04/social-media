1a424e39e149187b1aaa517d19ffb038da1b8849
Truc Pham <trucpham04@proton.me>
2026-03-19 15:13:16 +0700

    feat(auth): add OpenAPI documentation for user registration, login, and token refresh endpoints
    - Replaced JWT token views with custom LoginView and RefreshView for better control.
    - Added OpenAPI documentation for user registration, login, and token refresh endpoints.
    - Updated permission classes to allow access for all users.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-from .views import RegisterView
+from .views import RegisterView, LoginView, RefreshView
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("token/refresh/", RefreshView.as_view(), name="token_refresh"),
 ]
 
